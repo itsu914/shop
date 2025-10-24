2a7c4cacea44fef42b54c9060d2d9daa8d9c3984
itsu914 <takedaitsuki914@gmail.com>
2025-10-24 17:02:39 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,22 @@
+// js/auth.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA7HCpcNlKaGkVv4rjbYP5XYkbfTH8KXJY",
+    authDomain: "sutoer-5cab5.firebaseapp.com",
+    projectId: "sutoer-5cab5"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// ログイン用
+export function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+}
+
+// アカウント作成用
+export function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+}
